87c872d1dd71753347c937159c21d063d9c880fc
ELK-milu <2636626273@qq.com>
2025-09-25 22:33:17 +0800

    修改automatic上传策略

--- utils/difyknowledgebase.py
+++ utils/difyknowledgebase.py
@@ -1,6 +1,6 @@
 import requests
 
-from utils import base_url,key,id
+from utils import base_url,id,key
 
 
 def get_database_info():
@@ -55,33 +55,32 @@ def database_post(title, content, database_info):
             ],
             "segmentation": {
                 "separator": "###",
-                "max_tokens": 1000
+                "max_tokens": 1000,
+                "chunk_overlap": 150
             },
             "mode": "automatic"
         }
 
-        payload = {
-            "name": title,
-            "text": content,
-            "indexing_technique": database_info.get("indexing_technique", "high_quality"),
-            "doc_form": database_info.get("doc_form", "hierarchical_model"),
-            "doc_language": "Chinese",
-            "process_rule": process_rule,
-            "retrieval_model": {
-                "search_method": retrieval_model_info.get("search_method", "hybrid_search"),
-                "reranking_enable": retrieval_model_info.get("reranking_enable", True),
-                "reranking_mode": reranking_model_info.get("reranking_model_name", "BAAI/bge-reranker-v2-m3"),
-                "top_k": retrieval_model_info.get("top_k", 3),
-                "score_threshold_enabled": retrieval_model_info.get("score_threshold_enabled", False),
-                "score_threshold": retrieval_model_info.get("score_threshold", 0),
-                "weights": retrieval_model_info.get("weights", None)
-            },
-            "embedding_model_provider": database_info.get("embedding_model_provider",
-                                                          "langgenius/siliconflow/siliconflow"),
-            "embedding_model": database_info.get("embedding_model", "BAAI/bge-m3")
 
-        }
-
+    payload = {
+        "name": title,
+        "text": content,
+        "indexing_technique": database_info.get("indexing_technique", "high_quality"),
+        "doc_form": database_info.get("doc_form", "hierarchical_model"),
+        "doc_language": "Chinese",
+        "process_rule": process_rule,
+        "retrieval_model": {
+            "search_method": retrieval_model_info.get("search_method", "hybrid_search"),
+            "reranking_enable": retrieval_model_info.get("reranking_enable", True),
+            "reranking_mode": reranking_model_info.get("reranking_model_name", "BAAI/bge-reranker-v2-m3"),
+            "top_k": retrieval_model_info.get("top_k", 3),
+            "score_threshold_enabled": retrieval_model_info.get("score_threshold_enabled", False),
+            "score_threshold": retrieval_model_info.get("score_threshold", 0),
+            "weights": retrieval_model_info.get("weights", None)
+        },
+        "embedding_model_provider": database_info.get("embedding_model_provider", "langgenius/siliconflow/siliconflow"),
+        "embedding_model": database_info.get("embedding_model", "BAAI/bge-m3")
+    }
 
     headers = {
         "Authorization": f"Bearer {key}",
@@ -90,7 +89,6 @@ def database_post(title, content, database_info):
 
     response = requests.post(url, json=payload, headers=headers)
     print(response.json())
-
 
 def datebase_post_pipeline(title, content):
     database_info_response = get_database_info()  # 假设这个函数返回响应数据
